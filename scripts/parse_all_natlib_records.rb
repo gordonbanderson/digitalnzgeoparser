279519f1ec7ad5ff9ee1e00d32733721723d28be
Your Name <gordon.b.anderson@gmail.com>
2009-11-02 17:20:35 +0700

    Geoparsing: Parse all script now only does those not already parsed, newest first

--- scripts/parse_all_natlib_records.rb
+++ scripts/parse_all_natlib_records.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ../digitalnz/script/runner
 
-for nl in NatlibMetadata.find(:all, :order => 'id desc')
+for nl in NatlibMetadata.find_by_sql("select * from natlib_metadatas
+where id not in (select natlib_metadata_id from submissions) order by id desc")
+  
   puts "ruby geoparse_record.rb  #{nl.natlib_id}"
 end
