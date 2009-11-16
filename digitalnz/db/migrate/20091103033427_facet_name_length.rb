0127e43a8b393c9bbfab9d546e4936352d3abbd4
Gordon Anderson <gordon@Gordon-Andersons-MacBook.local>
2009-11-16 17:21:57 +0700

    Migrations: Removed transaction and removed need to change sequences if last item blank (ie none) - issues discovered during fresh install

--- digitalnz/db/migrate/20091103033427_facet_name_length.rb
+++ digitalnz/db/migrate/20091103033427_facet_name_length.rb
@@ -2,7 +2,7 @@ class FacetNameLength < ActiveRecord::Migration
   def self.up
     rename_table :facet_fields, :facet_fields_old
     sql = ActiveRecord::Base.connection()
-    sql.begin_db_transaction
+    #sql.begin_db_transaction
     create_table :facet_fields do |t|
       t.text :name
       t.integer :parent_id
@@ -11,7 +11,7 @@ class FacetNameLength < ActiveRecord::Migration
     end
     
     sql.execute("INSERT INTO facet_fields SELECT * FROM facet_fields_old")
-    sql.commit_db_transaction
+    #sql.commit_db_transaction
     
   end
 
