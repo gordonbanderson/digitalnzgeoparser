d1c894f1c2980cf469a06c88614feba19aa466a5
Gordon Anderson <gordon@Gordon-Andersons-MacBook.local>
2009-11-20 19:27:43 +0700

    Deal with null display URL - looks like a bug

--- digitalnz/app/helpers/archive_searches_helper.rb
+++ digitalnz/app/helpers/archive_searches_helper.rb
@@ -2,8 +2,12 @@ module ArchiveSearchesHelper
   
   #Find the host of a given URL
   def hostof(url)
-    stripped = url.gsub("http://", '')
-    parts = stripped.split('/')
-    parts[0]
+    result = 'NOT FOUND'
+    if !url.blank?
+      stripped = url.gsub("http://", '')
+      parts = stripped.split('/')
+      result = parts[0]
+    end
+    result
   end
 end
