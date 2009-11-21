6950f85d842ec3b380c2031ac94a3e8ee07a29cb
Gordon Anderson <gordon@Gordon-Andersons-MacBook.local>
2009-11-21 11:46:08 +0700

    Geoparsing: Now batched

--- scripts/parse_all_natlib_records.rb
+++ scripts/parse_all_natlib_records.rb
@@ -1,7 +1,16 @@
 #!/usr/bin/env ../digitalnz/script/runner
-
+c = 0
+current = []
 for nl in NatlibMetadata.find_by_sql("select * from natlib_metadatas
 where id not in (select natlib_metadata_id from submissions) order by id desc")
-  
-  puts "ruby geoparse_record.rb  #{nl.natlib_id}"
+  current << nl.natlib_id
+  c = c+1
+  if c == 4
+    c = 0
+    puts "ruby geoparse_record.rb  #{current.join(' ')}"
+    current = []
+  end
+ 
 end
+
+puts "ruby geoparse_record.rb  #{current.join(' ')}"
