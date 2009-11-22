b6e4d8b39ce680013def25b0dec0b604f544c168
Gordon Anderson <gordon@Gordon-Andersons-MacBook.local>
2009-11-22 13:22:39 +0700

    Area rendering: Moved method to submissions helper

--- digitalnz/app/helpers/submissions_helper.rb
+++ digitalnz/app/helpers/submissions_helper.rb
@@ -1,2 +1,9 @@
 module SubmissionsHelper
+    def self.pretty_area(area)
+      result = 'No area defined'
+      if !area.blank?
+        result = area.round 
+      end
+      result
+    end
 end
