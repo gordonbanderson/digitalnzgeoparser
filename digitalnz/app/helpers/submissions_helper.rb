d61f09ac77a3a795cf4f8e22ec1bde23254d918b
Gordon Anderson <gordon@Gordon-Andersons-MacBook.local>
2009-11-25 13:21:37 +0700

    Area: Now has significant figures for low values (eg 0.84km squared)

--- digitalnz/app/helpers/submissions_helper.rb
+++ digitalnz/app/helpers/submissions_helper.rb
@@ -1,9 +1,23 @@
 module SubmissionsHelper
     def self.pretty_area(area)
       result = 'No area defined'
+      
+      rounded_area = area.round.to_s
+      if area < 10
+          rounded_area = number_to_n_significant_digits(area,2)
+      elsif area < 100
+          rounded_area = number_to_n_significant_digits(area,2)
+          
+      end
+      
       if !area.blank?
-        result = area.round 
+        result = "#{rounded_area} km&sup2;"
       end
       result
+    end
+    
+    
+    def self.number_to_n_significant_digits(number, n = 3)
+    ("%f"%[("%.#{n}g"%number).to_f]).sub(/\.?0*\z/,'')
     end
 end
