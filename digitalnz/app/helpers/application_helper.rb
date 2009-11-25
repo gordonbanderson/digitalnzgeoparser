6137acf35ab037c9c675ecbe7b605c4e8c2322aa
Gordon Anderson <gordon@Gordon-Andersons-MacBook.local>
2009-11-25 14:38:58 +0700

    SEO: Added seo method for nicer URLs

--- digitalnz/app/helpers/application_helper.rb
+++ digitalnz/app/helpers/application_helper.rb
@@ -1,3 +1,16 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+    
+    #Make a friendly SEO URL
+    def seo_id(id,phrase)
+        result = id.to_s
+        result << '-'
+        for part in phrase.parameterize.split('-')
+            result << part
+            result << '-'
+            break if result.length > 50
+        end
+        result[-1]='' if result[-1]='-'
+        result
+    end
 end
