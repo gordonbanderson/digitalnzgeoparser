cf1b699eec7b2b57e46fc606bd68e8459fb9c8fa
Gordon Anderson <gordon@Gordon-Andersons-MacBook.local>
2009-12-02 14:32:33 +0700

    Sequence fix ignored for mysql as not required

--- digitalnz/db/migrate/20091103034737_fixsequences.rb
+++ digitalnz/db/migrate/20091103034737_fixsequences.rb
@@ -8,9 +8,19 @@ class Fixsequences < ActiveRecord::Migration
     f = FacetField.find(:last)
     n = NatlibMetadata.find(:last)
     
-    #Do not alter the sequences if we are creating from scratch (ie a first install migration)
-    sql.execute("alter sequence natlib_metadatas_id_seq1 restart #{1+n.id};") if !n.blank?
-    sql.execute("alter sequence facet_fields_id_seq1 restart #{1+f.id}") if !f.blank?
+    puts f.to_yaml
+    puts n.to_yaml
+    
+    class_name = ActiveRecord::Base.connection().class
+    
+    #MYSQL seems to have the right id values automatically
+    if class_name == 'ActiveRecord::ConnectionAdapters::MysqlAdapter:Class'
+        #Do not alter the sequences if we are creating from scratch (ie a first install migration)
+        sql.execute("alter sequence natlib_metadatas_id_seq1 restart #{1+n.id};") if !n.blank?
+        sql.execute("alter sequence facet_fields_id_seq1 restart #{1+f.id}") if !f.blank?
+    end
+    
+
     #sql.commit_db_transaction
   end
 
