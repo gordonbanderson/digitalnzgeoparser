632d678f4086e2bffa5cf146a98769c1603c619d
Gordon Anderson <gordon@Gordon-Andersons-MacBook.local>
2009-12-02 10:43:05 +0700

    Tests: Added tests for creation of bounding boxes and tests for country bias

--- digitalnz/test/unit/cached_geo_search_test.rb
+++ digitalnz/test/unit/cached_geo_search_test.rb
@@ -46,17 +46,66 @@ class CachedGeoSearchTest < ActiveSupport::TestCase
     
   end
   
-  #Test for bug where only one result was being returned
-  def test_lots_of_results
-      locations = parse_address("Springfield")
+
+  #Test NZ country bias (same as default)
+  def test_springfield_nz_country_bias
+      CACHE.flush_all
+      search_terms = check_location_count_search_terms "Springfield",1,'nz'
+      assert_equal search_terms[0].country, 'New Zealand'
+  end
+  
+  def test_springfield_uk_country_bias
+      CACHE.flush_all
+      search_terms = check_location_count_search_terms "Springfield",1,'uk'
+      assert_equal search_terms[0].country, 'United Kingdom'
+  end
+  
+  #Test USA country bias
+  def test_springfield_us_country_bias
+      CACHE.flush_all
+      search_terms = check_location_count_search_terms "Springfield",10,'us'
       
-      search_terms = CachedGeoSearchTerm.find_by_search_term("Springfield").cached_geo_searches
-      for res in search_terms
-         puts res.to_yaml
-         puts "======" 
+      for st in search_terms
+          assert_equal 'USA',st.country
+          
       end
-      assert_equal 12, search_terms.length
+  end
+  
+  
+  #Eastbourne defaults to the UK unfortunately
+  def test_eastbourne
+      search_terms = check_location_count_search_terms "Eastbourne",1,'nz'
+      assert_equal 'New Zealand',search_terms[0].country
       
+  end
+  
+  
+  def test_springfield_no_country_bias
+      CACHE.flush_all
+      search_terms = check_location_count_search_terms "Springfield",1,'uk'
+      assert_equal 'New Zealand', search_terms[0].country
+  end
+  
+  
+  #Check that bounding boxes are correctly saved
+  def test_bounding_boxes
+      search_terms = check_location_count_search_terms "Wellington",1,'nz'
+      wellington = search_terms[0]
+      assert_equal 'New Zealand', wellington.country 
+      puts wellington.to_yaml
+      assert_equal -41.2948638, wellington.bbox_south
+      assert_equal -41.2780951, wellington.bbox_north
+      assert_equal 174.7602096, wellington.bbox_west
+      assert_equal 174.7922244, wellington.bbox_east
+      
+      assert wellington.bbox_north > wellington.bbox_south
+      assert wellington.bbox_east > wellington.bbox_west
+
+     #Check lat/lon in bounding box
+      assert wellington.bbox_west < wellington.longitude
+      assert wellington.bbox_east > wellington.longitude
+      assert wellington.bbox_south < wellington.latitude
+      assert wellington.bbox_north > wellington.latitude
   end
   
   
@@ -104,6 +153,24 @@ class CachedGeoSearchTest < ActiveSupport::TestCase
   
   def test_small_search
     result = parse_address("A")
+  end
+  
+  
+  
+  def check_location_count_search_terms(location_name, expected_amount, bias)
+      puts "Searching for #{location_name} with bias of #{bias}"
+      locations = parse_address(location_name, bias)
+      
+      search_terms = CachedGeoSearchTerm.find_by_search_term(location_name).cached_geo_searches
+      assert_equal expected_amount, search_terms.length
+      search_terms
+  end
+  
+  
+  
+  def test_same_location
+     #Wellington Grammar School, Clifton Terrace, Wellington
+     #Wellington Grammar School,  Wellington 
   end
   
   
