e670986ed0ce739cd504a2f73a8fcaa26bfcae6b
Gordon Anderson <gordon@Gordon-Andersons-MacBook.local>
2009-12-05 21:45:27 +0700

    Testing: Initial shell of record geoparser test

---
+++ digitalnz/test/unit/geoparse_record_helper_test.rb
@@ -0,0 +1,47 @@
+
+require File.dirname(__FILE__) + '/../test_helper'
+require File.dirname(__FILE__) + '/../../app/helpers/geoparse_record_helper'
+
+class GeoparseRecordHelperTest < Test::Unit::TestCase
+
+    include GeoparseRecordHelper
+    
+    
+    def test_calais_80282
+        parse_natlib_record(80282)
+    end
+    
+end
+
+=begin
+
+118333
+
+
+Person: 
+- Sydney Charles Smith
+City: 
+- Wellington
+NaturalFeature: 
+- Houghton Bay
+Relations:
+
+
+
+80282
+
+Person: 
+- Frank Arnold Coxhead
+Facility: 
+- Tay Street
+Country: 
+- New Zealand
+Relations: 
+- ""
+Company: 
+- Bank of New Zealand
+- Union Bank of Australia
+CALAIS TAGS:
+
+
+=end
