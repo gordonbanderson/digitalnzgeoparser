e1b9322d73113f29aecf2d522758a93b12c6bceb
Gordon Anderson <gordon@Gordon-Andersons-MacBook.local>
2009-12-05 22:07:54 +0700

    Tests: Added check for calais parent category

--- digitalnz/test/unit/geoparse_record_helper_test.rb
+++ digitalnz/test/unit/geoparse_record_helper_test.rb
@@ -11,6 +11,16 @@ class GeoparseRecordHelperTest < ActiveRecord::TestCase
     
     def test_calais_80282
         parse_natlib_record(80282)
+        cws = CalaisWord.find(:all).map{|c|c.word}
+        for cw in cws
+           puts "CALAIS_WORD:#{cw}"
+        end
+        
+        assert cws.include? 'Person'
+        assert cws.include? 'Position'
+        assert cws.include? 'Relations'
+        assert cws.include? 'Company'
+        assert cws.include? 'Country'
     end
     
 end
