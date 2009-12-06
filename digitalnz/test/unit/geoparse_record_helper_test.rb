5ad6daf18af361c562e8c32876055102db5c5937
Gordon Anderson <gordon@Gordon-Andersons-MacBook.local>
2009-12-06 10:40:22 +0700

    Tests: Added assertion that child calais word exists, which is needing fixed

--- digitalnz/test/unit/geoparse_record_helper_test.rb
+++ digitalnz/test/unit/geoparse_record_helper_test.rb
@@ -16,11 +16,16 @@ class GeoparseRecordHelperTest < ActiveRecord::TestCase
            puts "CALAIS_WORD:#{cw}"
         end
         
+        #Top level calais words
         assert cws.include? 'Person'
         assert cws.include? 'Position'
         assert cws.include? 'Relations'
         assert cws.include? 'Company'
         assert cws.include? 'Country'
+        
+        
+        #Second level
+        assert cws.include? 'Tay Street'
     end
     
 end
