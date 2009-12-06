c37c03a423c70fda6913d0d248bed4581c563623
Gordon Anderson <gordon@Gordon-Andersons-MacBook.local>
2009-12-06 12:49:57 +0700

    Models: Fixed rel for calais entry

--- digitalnz/app/models/calais_entry.rb
+++ digitalnz/app/models/calais_entry.rb
@@ -1,5 +1,11 @@
 class CalaisEntry < ActiveRecord::Base
-  belongs_to :calais_submission
-  belongs_to :calais_child_word
-  belongs_to :calais_parent_word
+  belongs_to :child_word, :class_name => 'CalaisWord', :foreign_key => 'calais_child_word_id'
+  belongs_to :parent_word, :class_name => 'CalaisWord', :foreign_key => 'calais_parent_word_id'
+  
+  validates_presence_of :child_word, :parent_word
+  
+  #Print out for debug purposes
+  def pretty_print
+     "#{parent_word.word} ==> #{child_word.word}" 
+  end
 end
