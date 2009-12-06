b5e4593707d4e4239ebc8c48097b1226d8efce19
Gordon Anderson <gordon@Gordon-Andersons-MacBook.local>
2009-12-06 14:09:17 +0700

    Models: Changed calais entry to be hatbm with submissions, added helper method to get previous version

--- digitalnz/app/models/calais_entry.rb
+++ digitalnz/app/models/calais_entry.rb
@@ -2,7 +2,20 @@ class CalaisEntry < ActiveRecord::Base
   belongs_to :child_word, :class_name => 'CalaisWord', :foreign_key => 'calais_child_word_id'
   belongs_to :parent_word, :class_name => 'CalaisWord', :foreign_key => 'calais_parent_word_id'
   
+  #These pairings belong to submitted texts
+  has_and_belongs_to_many :submissions
+  
   validates_presence_of :child_word, :parent_word
+  
+  #Only create a new one if none exists, otehrwise use an existing combo
+  def self.find_or_create(parent_calais_word, child_calais_word)
+      calais_entry = CalaisEntry.find(:first, :conditions => ["calais_child_word_id = ? and calais_parent_word_id = ?", child_calais_word.id, parent_calais_word.id])
+      if calais_entry.blank?
+          calais_entry = CalaisEntry::create :child_word => child_calais_word, :parent_word => parent_calais_word
+      end
+      
+      calais_entry
+  end
   
   #Print out for debug purposes
   def pretty_print
