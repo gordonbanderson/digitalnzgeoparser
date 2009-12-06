0c97a930909ad0a2339a5fa23f75a184e4cc88dd
Gordon Anderson <gordon@Gordon-Andersons-MacBook.local>
2009-12-06 12:50:20 +0700

    Models: Further work in fixing calais

--- digitalnz/app/models/calais_word.rb
+++ digitalnz/app/models/calais_word.rb
@@ -1,4 +1,20 @@
 class CalaisWord < ActiveRecord::Base
-  has_one :calais_parent_word
-  has_one :calais_child_word
+    #Permalink for grouping on webpages
+    has_permalink :word, :update => true
+
+    has_one :calais_parent_word
+    has_one :calais_child_word
+    
+    #Validations
+    validates_presence_of :word
+    validates_uniqueness_of :word
+  
+  
+  def self.find_or_create word
+    cw = CalaisWord.find_by_word(word)
+    if cw.blank?
+      cw = CalaisWord::create :word => word  
+    end
+    cw  
+  end
 end
