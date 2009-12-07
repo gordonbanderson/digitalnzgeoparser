71d1ae4e3ca587e63fb5fb007a5bb6879589769d
Gordon Anderson <gordon@Gordon-Andersons-MacBook.local>
2009-12-07 14:29:48 +0700

    Tests: Publisher test now passing

--- digitalnz/test/unit/geoparse_record_helper_test.rb
+++ digitalnz/test/unit/geoparse_record_helper_test.rb
@@ -86,6 +86,20 @@ class GeoparseRecordHelperTest < ActiveRecord::TestCase
     end
     
     
+    #Test that publishers are stored correctly in an hatbm table
+    def test_publisher
+       parse_natlib_record 1188492
+       n = NatlibMetadata.find_by_natlib_id 1188492
+       publishers = n.publishers
+       
+       puts Publisher.find(:all).to_yaml
+       
+       assert_equal 1, publishers.length
+       assert_equal "University of Canterbury Library", publishers[0].name
+       assert_equal "university-of-canterbury-library", publishers[0].permalink
+    end
+    
+    
     def test_nil_parsing_error
        #86556 
     end
