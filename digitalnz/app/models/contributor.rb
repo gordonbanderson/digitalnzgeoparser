d788c706ca91c456710bd71aad6c5d149760fc5c
Gordon Anderson <gordon@Gordon-Andersons-MacBook.local>
2009-12-07 14:11:25 +0700

    Models: Addition of contributor model

---
+++ digitalnz/app/models/contributor.rb
@@ -0,0 +1,12 @@
+class Contributor < ActiveRecord::Base
+    has_permalink :name, :update => true
+    has_and_belongs_to_many :natlib_metadatas
+    
+    validates_uniqueness_of :name
+    
+    def self.find_or_create name
+      cw = Contributor.find_by_name(name)
+      cw = Contributor::create :name => name   if cw.blank?
+      cw  
+    end
+end
