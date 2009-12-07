4f6ae5332e053b43339f77f98d8542cb7af6182a
Gordon Anderson <gordon@Gordon-Andersons-MacBook.local>
2009-12-07 14:39:52 +0700

    Models: New collectio model

---
+++ digitalnz/app/models/collection.rb
@@ -0,0 +1,13 @@
+class Collection < ActiveRecord::Base
+    has_permalink :name, :update => true
+    has_and_belongs_to_many :natlib_metadatas
+    
+    validates_uniqueness_of :name
+    
+    def self.find_or_create name
+      cw = Collection.find_by_name(name)
+      cw = Collection::create :name => name   if cw.blank?
+      cw  
+    end
+end
+
