cea263c3ea7edb29b69b19761bc3b677262b1897
Gordon Anderson <gordon@Gordon-Andersons-MacBook.local>
2009-12-07 14:49:33 +0700

    Models: Addition of language

---
+++ digitalnz/app/models/language.rb
@@ -0,0 +1,12 @@
+class Language < ActiveRecord::Base
+    has_permalink :name, :update => true
+    has_and_belongs_to_many :natlib_metadatas
+    
+    validates_uniqueness_of :name
+    
+    def self.find_or_create name
+      cw = Language.find_by_name(name)
+      cw = Language::create :name => name   if cw.blank?
+      cw  
+    end
+end
