49726815a954d4a636815f7b7b4d7c562a404058
Gordon Anderson <gordon@Gordon-Andersons-MacBook.local>
2009-12-07 13:54:21 +0700

    Model: Addition of content partner separated from natlib metadatas

--- digitalnz/app/models/content_partner.rb
+++ digitalnz/app/models/content_partner.rb
@@ -1,2 +1,13 @@
 class ContentPartner < ActiveRecord::Base
+    has_permalink :name, :update => true
+    has_and_belongs_to_many :natlib_metadatas
+    
+    validates_uniqueness_of :name
+    
+    
+    def self.find_or_create name
+      cw = ContentPartner.find_by_name(name)
+      cw = ContentPartner::create :name => name   if cw.blank?
+      cw  
+    end
 end
