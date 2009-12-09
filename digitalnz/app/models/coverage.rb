f81886dacc1944d8cc18f29328a7398ebf4eea1d
Gordon Anderson <gordon@Gordon-Andersons-MacBook.local>
2009-12-09 20:12:08 +0700

    Models: Added find or create to coverage in order to deal with not duplicating coverage names

--- digitalnz/app/models/coverage.rb
+++ digitalnz/app/models/coverage.rb
@@ -2,5 +2,11 @@ class Coverage < ActiveRecord::Base
     has_permalink :name, :update => true
     has_and_belongs_to_many :natlib_metadatas
     
+    def self.find_or_create the_name
+      o = Coverage.find_by_name the_name
+      o = Coverage::create :name => the_name   if o.blank?
+      o  
+    end
+    
 
 end
