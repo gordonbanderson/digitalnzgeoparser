73f9de8f21f2a7f601d350f17388e28e57083f5e
Gordon Anderson <gordon@Gordon-Andersons-MacBook.local>
2009-12-09 20:01:49 +0700

    Migrations: Added unique indexes for names, enforcing this at db level as well as rails

--- digitalnz/db/migrate/20091208152031_avoid_duplicates_with_indexes.rb
+++ digitalnz/db/migrate/20091208152031_avoid_duplicates_with_indexes.rb
@@ -1,27 +1,40 @@
 class AvoidDuplicatesWithIndexes < ActiveRecord::Migration
+  
+  #Enforce unique names at the database level
   def self.up
-      add_index :subjects, :name, :unique => true
+      add_index :subjects, :name, :unique => true #Tested
+      add_index :collections, :name, :unique => true
+      add_index :coverages, :name, :unique => true      
+      add_index :creators, :name, :unique => true
+      add_index :contributors, :name, :unique => true
+      add_index :languages, :name, :unique => true
+      add_index :placenames, :name, :unique => true
+      
+      add_index :publishers, :name, :unique => true
+      add_index :formats, :name, :unique => true
+      add_index :identifiers, :name, :unique => true
+      
+      #DONE ALREADY add_index :calais_words, :word, :unique => true
+      add_index :categories, :name, :unique => true
+      add_index :content_partners, :name, :unique => true
+      #DONE ALREADY add_index :phrases, :words, :unique => true
+      add_index :relations, :name, :unique => true
+      add_index :rights, :name, :unique => true      
 
-=begin
-add_index :cached_geo_searches, :permalink, :unique => true
-add_index :calais_words, :permalink, :unique => true
-add_index :categories, :permalink, :unique => true
-add_index :collections, :permalink, :unique => true
-add_index :content_partners, :permalink, :unique => true
-add_index :contributors, :permalink, :unique => true
-add_index :coverages, :permalink, :unique => true
-add_index :creators, :permalink, :unique => true
-add_index :formats, :permalink, :unique => true
-add_index :identifiers, :permalink, :unique => true
-add_index :languages, :permalink, :unique => true
-add_index :phrases, :permalink, :unique => true
-add_index :placenames, :permalink, :unique => true
-add_index :publishers, :permalink, :unique => true
-add_index :relations, :permalink, :unique => true
-add_index :rights, :permalink, :unique => true
-=end
   end
 
   def self.down
+      remove_index :subjects, :name
+      remove_index :collections, :name
+      remove_index :coverages, :name
+      
+      remove_index :creators, :name
+      remove_index :contributors, :name
+      remove_index :languages, :name
+      remove_index :placenames, :name
+      
+      remove_index :publishers, :name
+      remove_index :formats, :name
+      remove_index :identifiers, :name
   end
 end
