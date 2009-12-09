db741fc423b36999d5a440c4eba4114c8d464c3b
Gordon Anderson <gordon@Gordon-Andersons-MacBook.local>
2009-12-09 22:31:47 +0700

    Added uniqueness check and creation of unique name method

--- digitalnz/app/models/placename.rb
+++ digitalnz/app/models/placename.rb
@@ -1,4 +1,13 @@
 class Placename < ActiveRecord::Base
     has_and_belongs_to_many :natlib_metadatas
-    has_permalink :name, :update => true  
+    has_permalink :name, :update => true
+    
+    validates_uniqueness_of :name
+    
+    
+    def self.find_or_create the_name
+      o = Placename.find_by_name the_name
+      o = Placename::create :name => the_name   if o.blank?
+      o  
+    end
 end
