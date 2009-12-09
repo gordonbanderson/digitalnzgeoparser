86c99be836d6500562c0ce0ec278006f4ee8e493
Gordon Anderson <gordon@Gordon-Andersons-MacBook.local>
2009-12-09 22:12:35 +0700

    Models: Added duplication avoidance method to relation

--- digitalnz/app/models/relation.rb
+++ digitalnz/app/models/relation.rb
@@ -1,4 +1,13 @@
 class Relation < ActiveRecord::Base
     has_and_belongs_to_many :natlib_metadatas
-    has_permalink :name, :update => true  
+    has_permalink :name, :update => true
+    
+    validates_uniqueness_of :name
+    
+    
+    def self.find_or_create the_name
+      o = Relation.find_by_name the_name
+      o = Relation::create :name => the_name   if o.blank?
+      o  
+    end  
 end
