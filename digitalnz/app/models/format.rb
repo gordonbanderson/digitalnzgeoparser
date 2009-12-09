faa7ad87ec434548822445db7b51a3c1e1b78441
Gordon Anderson <gordon@Gordon-Andersons-MacBook.local>
2009-12-09 21:20:18 +0700

    Added uniqueness check and method to get uniquely named format, or create one

--- digitalnz/app/models/format.rb
+++ digitalnz/app/models/format.rb
@@ -1,4 +1,13 @@
 class Format < ActiveRecord::Base
     has_and_belongs_to_many :natlib_metadatas
     has_permalink :name, :update => true
+    
+    validates_uniqueness_of :name
+    
+    
+    def self.find_or_create the_name
+      o = Format.find_by_name the_name
+      o = Format::create :name => the_name   if o.blank?
+      o  
+    end
 end
