4f1f5f379eb17094c706343604b06ced5e9c74a2
Gordon Anderson <gordon@Gordon-Andersons-MacBook.local>
2009-12-10 11:59:13 +0700

    Models: Category now has unique names enforced

--- digitalnz/app/models/category.rb
+++ digitalnz/app/models/category.rb
@@ -2,4 +2,13 @@ class Category < ActiveRecord::Base
   has_and_belongs_to_many :natlib_metadatas
   has_permalink :name, :update => true
   
+  validates_uniqueness_of :name
+  
+  
+  def self.find_or_create the_name
+    o = Category.find_by_name the_name
+    o = Category::create :name => the_name   if o.blank?
+    o  
+  end
+  
 end
