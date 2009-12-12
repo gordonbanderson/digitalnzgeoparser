060be821e1c3c8442c3efbb8b6d2bc31872b30ee
Gordon Anderson <gordon@Gordon-Andersons-MacBook.local>
2009-12-12 16:11:48 +0700

    Tipes now have a permalink

---
+++ digitalnz/db/migrate/20091212090637_tipes_permalink.rb
@@ -0,0 +1,12 @@
+class TipesPermalink < ActiveRecord::Migration
+  def self.up
+      add_column :tipes, :permalink, :string
+      for tipe in Tipe.find(:all)
+         tipe.save! 
+      end
+  end
+
+  def self.down
+      remove_colum :tipes, :permalink
+  end
+end
