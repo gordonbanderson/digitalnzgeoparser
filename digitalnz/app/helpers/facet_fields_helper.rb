92fb9bdb733e661196151f50312f01523ea80385
Gordon Anderson <gordon@Gordon-Andersons-MacBook.local>
2009-12-12 18:58:09 +0700

    WIP: Adding method to parse facet fields

--- digitalnz/app/helpers/facet_fields_helper.rb
+++ digitalnz/app/helpers/facet_fields_helper.rb
@@ -1,2 +1,21 @@
 module FacetFieldsHelper
+    
+    def parse_facet_params(facet_params)
+        key = false
+        last_key = facet_params[0]
+        facet_hash = {}
+        for i in 1..facet_params.length
+            if !key
+                facet_hash[last_key] = facet_params[i]
+            else
+                  last_key = facet_params[i]
+           end
+
+           key = !key
+           logger.debug "#{key} - #{last_key} - #{facet_params[i]}"
+           i = i + 1
+        end
+
+        facet_hash
+    end
 end
