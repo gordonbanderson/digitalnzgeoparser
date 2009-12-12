24553af3448076de591483c14ed7e8634d85ab7d
Gordon Anderson <gordon@Gordon-Andersons-MacBook.local>
2009-12-12 16:12:10 +0700

    Models: Types now have permalink and eager loading of some natlib metadata

--- digitalnz/app/models/tipe.rb
+++ digitalnz/app/models/tipe.rb
@@ -1,5 +1,7 @@
 class Tipe < ActiveRecord::Base
-  has_and_belongs_to_many :natlib_metadatas
+    has_permalink :name, :update => true
+    
+    has_and_belongs_to_many :natlib_metadatas, :include => [:contributors, :categories, :record_dates, :submission]
   
   validates_uniqueness_of :name
   
