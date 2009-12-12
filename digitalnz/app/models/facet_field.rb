51fe264159bdd29528e80b1e6ab429b7495041a1
Gordon Anderson <gordon@Gordon-Andersons-MacBook.local>
2009-12-12 19:48:45 +0700

    Models: Added method to create unique only version

--- digitalnz/app/models/facet_field.rb
+++ digitalnz/app/models/facet_field.rb
@@ -1,3 +1,13 @@
 class FacetField < ActiveRecord::Base
   acts_as_tree
+  
+  validates_uniqueness_of :name
+  has_permalink :name, :update => true
+  
+  def self.find_or_create the_name
+    o = FacetField.find_by_name the_name
+    o = FacetField::create :name => the_name   if o.blank?
+    o  
+  end
+  
 end
