673822f59eaa30c9ce83c0156fbabbc8ad146e7e
Gordon Anderson <gordon@Gordon-Andersons-MacBook.local>
2009-12-15 20:43:36 +0700

    Migrations: Fixes for change in facet field that broke clean migration

--- digitalnz/db/migrate/20091027103408_create_facet_fields.rb
+++ digitalnz/db/migrate/20091027103408_create_facet_fields.rb
@@ -1,5 +1,19 @@
 class CreateFacetFields < ActiveRecord::Migration
+    
+    #Definition of facet fields prior to adding permalink - the has permalink breaks this migration :(
+    class FacetField < ActiveRecord::Base;
+        acts_as_tree
+
+          validates_uniqueness_of :name, :scope => 'parent_id'
+    end
+    
+    
   def self.up
+      
+
+      
+      
+      
     create_table :facet_fields do |t|
       t.string :name
       t.integer :parent_id
--- digitalnz/db/migrate/20091212120733_facet_field_permalink.rb
+++ digitalnz/db/migrate/20091212120733_facet_field_permalink.rb
@@ -1,6 +1,9 @@
 class FacetFieldPermalink < ActiveRecord::Migration
   def self.up
-      #add_column :facet_fields, :permalink, :string
+      
+      FacetField.reset_column_information
+      
+      add_column :facet_fields, :permalink, :string
       for f in FacetField.find(:all)
          f.save! 
       end
