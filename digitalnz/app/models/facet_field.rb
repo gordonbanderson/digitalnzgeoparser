8ac2b2df7e8a4c40fc03f0b18f3627a2af2cef48
Gordon Anderson <gordon@Gordon-Andersons-MacBook.local>
2009-12-15 14:58:47 +0800

    Models: Changed scope of uniqueness of name in facet field to be the parent

--- digitalnz/app/models/facet_field.rb
+++ digitalnz/app/models/facet_field.rb
@@ -1,7 +1,7 @@
 class FacetField < ActiveRecord::Base
   acts_as_tree
   
-  validates_uniqueness_of :name
+  validates_uniqueness_of :name, :scope => 'parent_id'
   has_permalink :name, :update => true
   
   def self.find_or_create the_name
