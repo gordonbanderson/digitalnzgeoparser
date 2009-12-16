ccbe558dc84c182c3a5002027de4fab6237426b4
Gordon Anderson <gordon@Gordon-Andersons-MacBook.local>
2009-12-16 07:40:32 +0700

    Migrations: Removed spatial migrations temporarily due to dreamhost problems

--- digitalnz/db/migrate/20091214013529_spatial_point_cached_geo_searches.rb
+++ digitalnz/db/migrate/20091214013529_spatial_point_cached_geo_searches.rb
@@ -1,7 +1,10 @@
 class SpatialPointCachedGeoSearches < ActiveRecord::Migration
   def self.up
-      #add_column :cached_geo_searches, "geom", :point, :null=>false
-      #add_index :cached_geo_searches, "geom", :spatial=>true
+      #FIXME - switch to ISAM, separate table
+      #Currently the spatial indexing does not work on mysql  on dreamhost :(
+=begin
+      add_column :cached_geo_searches, "geom", :point, :null=>false
+      add_index :cached_geo_searches, "geom", :spatial=>true
       
       for cgs in CachedGeoSearch.find(:all)
           puts "SPATIAL SAVING:#{cgs.address} at #{cgs.latitude}, #{cgs.longitude}"
@@ -9,10 +12,13 @@ class SpatialPointCachedGeoSearches < ActiveRecord::Migration
          cgs.geom =  Point.from_x_y(cgs.longitude, cgs.latitude)
          cgs.save
       end
+=end
   end
 
   def self.down
+=begin
       remove_column :cached_geo_searches, :geom
       remove_index :cached_geo_searches_points
+=end
   end
 end
