1a8cccb6a39e26e5dfa8d8111e1c1cf316ee366f
Gordon Anderson <gordon@Gordon-Andersons-MacBook.local>
2009-12-17 13:41:50 +0700

    Schema changes

--- digitalnz/db/schema.rb
+++ digitalnz/db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091212120733) do
+ActiveRecord::Schema.define(:version => 20091216144359) do
 
   create_table "accuracies", :force => true do |t|
     t.column "name", :string
