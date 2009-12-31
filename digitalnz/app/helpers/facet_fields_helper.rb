d7d4b623056e7a0393e238c493c0d37e069da22c
Gordon Anderson <gordon@Gordon-Andersons-MacBook.local>
2009-12-31 10:41:30 +0700

    Facets: Create nice urls from list of facets, and order the keys

--- digitalnz/app/helpers/facet_fields_helper.rb
+++ digitalnz/app/helpers/facet_fields_helper.rb
@@ -32,6 +32,28 @@ module FacetFieldsHelper
     end
     
     
+    #Convert a list of facet ids to the nice url format, e.g. /category/images/collection/flickr
+    #@param facets mapping of filtername -> facet object
+    #@param new_filter_facet the extra filter being applied
+    def self.facets_to_seo_url(facets, new_filter_facet)
+        result = ''
+        
+        keys = facets.keys
+        keys << new_filter_facet.permalink
+        
+        for key in keys.sort
+            facet = facets[key]
+            if facet.blank?
+               facet = new_filter_facet 
+            end
+            if !facet.parent.blank?
+                result << "/#{facet.parent.permalink}/#{facet.permalink}"  
+            end
+        end
+        result
+    end
+    
+    
     #Convert the facet name into singular or plural
     def self.parent_facet_name(lower_case, pluralize=false)
        result = lower_case.camelize
