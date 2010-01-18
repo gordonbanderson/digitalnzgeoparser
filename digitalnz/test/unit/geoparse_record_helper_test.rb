b9e7fc905750777671facbdeb7fd388deae9e975
Gordon Anderson <gordon@Gordon-Andersons-MacBook.local>
2010-01-18 22:09:01 +0700

    Tests: Added test for Kohukohu School which is being removed erroneously by Open Calais

--- digitalnz/test/unit/geoparse_record_helper_test.rb
+++ digitalnz/test/unit/geoparse_record_helper_test.rb
@@ -606,6 +606,21 @@ class GeoparseRecordHelperTest < ActiveRecord::TestCase
         puts "--------SUBMISSION--------"
        
     end
+    
+    
+    
+    #Check for a record including Kohokohu school, that is not picking up the town
+    def test_kohukohu
+       parse_natlib_record 43988
+       s = Submission.find(:last)
+       
+       items = n.placenames       
+       names = items.map{|c| c.name}
+       perms = items.map{|i| i.permalink}
+       assert names.include? 'Kohukohu School'
+       
+       assert_equal 5, names.length
+    end
 end
 
 =begin
